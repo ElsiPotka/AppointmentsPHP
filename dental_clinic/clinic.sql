971a9dd488b253d3a6539adf11d7a8399dead26a
ElsiPotka <elsipotk@gmail.com>
2024-05-06 11:59:07 +0200

    Initial commit

---
+++ dental_clinic/clinic.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE IF NOT EXISTS appointments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    service VARCHAR(255) NOT NULL,
+    doctor VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    appointment_date DATE NOT NULL,
+    appointment_time TIME NOT NULL
+);
